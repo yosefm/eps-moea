02c805167ba491d97636b6c7da328a0d01be9f8a
Yosef Meller <mellerf@netvision.net.il>
2009-05-14 16:58:55 +0300

    Added test function TZD3

--- py_eps_moea/test_functions.py
+++ py_eps_moea/test_functions.py
@@ -5,7 +5,7 @@
 # Arguments: 
 # contenders - those whose fitness is to be evaluated
 
-from numpy import array, sqrt, hstack, atleast_2d
+from numpy import array, sqrt, hstack, atleast_2d, sin, pi
 
 def tau1(contenders):
     contenders = array(contenders, copy=False, ndmin=2)
@@ -21,4 +21,11 @@ def tau2(contenders):
     h = 1 - (fit1 / g)**2
     return hstack((fit1[:,None], (g*h)[:,None]))
 
-tzd = [tau1, tau2]
+def tau3(contenders):
+    contenders = array(contenders, copy=False, ndmin=2)
+    fit1 = contenders[:,0]
+    g = 1 + 9./(contenders.shape[1] - 1) * contenders[:,1:].sum(axis=1)
+    h = 1 - sqrt(fit1 / g) - (fit1 / g)*sin(pi*10*fit1)
+    return hstack((fit1[:,None], (g*h)[:,None]))
+
+tzd = [tau1, tau2, tau3]
