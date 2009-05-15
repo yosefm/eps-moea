ec73cf835e1b22b35c78e370b3b8d8210214bafc
Yosef Meller <mellerf@netvision.net.il>
2009-05-15 13:39:31 +0300

    Python code overhaul to newer C implementation.
    The Python version now more closely resembles the code at http://www.iitk.ac.in/kangal/codes/eps-moea/epsmoea-v1.0.tar, so now it is able to solve ZDT2, ZDT2.

--- py_eps_moea/creature.py
+++ py_eps_moea/creature.py
@@ -8,7 +8,8 @@ from numpy import random
 import numpy as N
 
 class Creature(object):
-    def __init__(self, low_bnds, up_bnds, mutation_chance, mutation_prm=20):
+    def __init__(self, low_bnds, up_bnds, mutation_chance, mutation_prm=20, \
+        recomb_chance=1.0, recomb_prm=15.):
         if len(low_bnds) != len(up_bnds):
             raise ValueError("Upper bounds number not equal to lower-bounds number.")
         if (low_bnds > up_bnds).any():
@@ -17,8 +18,12 @@ class Creature(object):
         self._up_bnds = up_bnds
         self._low_bnds = low_bnds
         self._ranges = up_bnds - low_bnds
+        
         self._p_mute = mutation_chance
         self._et_m = mutation_prm
+        
+        self._p_recomb = recomb_chance
+        self._et_c = recomb_prm
     
     def chromosome_len(self):
         return len(self._low_bnds)
@@ -55,6 +60,56 @@ class Creature(object):
         pop = random.random_sample((num_subjects, self.chromosome_len()))
         return self.denormalize(pop)
     
+    def sbx(self, mama, papa):
+        """Create an offspring using simulated binary crossover.
+        
+        Arguments: 
+        mama, papa - two breeders from the population, each a vector of genes.
+        
+        Returns: 
+        offsprings - a 2-tuple of length-g arrays, each with the genotype of an 
+            offspring after recombination and mutation.
+        """
+        offsprings = [mama.copy(), papa.copy()]
+        
+        if random.rand() <= self._p_recomb:
+            recombed = (random.rand(self.chromosome_len()) <= 0.5) & (abs(mama - papa) > 1e-14)
+            num_recombed = recombed.sum()
+            cum_dist = random.rand(2, num_recombed)
+            
+            parents = N.vstack((mama[recombed], papa[recombed]))
+            p_spread = abs(mama[recombed] - papa[recombed])
+            correction = 2 - (1 + N.vstack((parents.min(axis=0) - self._low_bnds[recombed], 
+                self._up_bnds[recombed] - parents.max(axis=0)))/ \
+                p_spread)**-(self._et_c + 1.)
+            
+            spread = N.empty((2, num_recombed))
+            cont_bool = cum_dist <= 1./correction
+            contracting = N.where(cont_bool)
+            expanding = N.where(~cont_bool)
+            spread[contracting] = \
+                (correction[contracting]*cum_dist[contracting])**(1./(self._et_c + 1))
+            spread[expanding] = \
+                (2 - correction[expanding]*cum_dist[expanding])**(-1./(self._et_c + 1))
+            
+            pm = random.random_integers(0, 1, num_recombed)
+            pm[pm == 0] = -1
+            recombed_traits = 0.5*(mama[recombed] + papa[recombed] + pm*spread*p_spread)
+            
+            underflow = N.where(recombed_traits < self._low_bnds[recombed])
+            recombed_traits[underflow] = N.tile(self._low_bnds[recombed],(2,1))[underflow]
+            overflow = N.where(recombed_traits > self._up_bnds[recombed])
+            recombed_traits[overflow] = N.tile(self._up_bnds[recombed],(2,1))[overflow]
+            
+            # Merge into not recombined traits from one of the parents:
+            which_recombed = random.random_integers(0, 1, num_recombed)
+            offsprings[0][recombed] = N.where(which_recombed, \
+                recombed_traits[0], recombed_traits[1])
+            offsprings[1][recombed] = N.where(which_recombed, \
+                recombed_traits[1], recombed_traits[0])
+            
+        return offsprings
+        
     def breed(self, mama, papa):
         """Creates a new genome for a subject by recombination of parent genes, and
         possibly mutation of the result, depending on the creature's mutation 
@@ -68,27 +123,34 @@ class Creature(object):
             recombination and mutation.
         """
         # Recombination place, using one-point crossover:
-        recomb = N.random.random_integers(0, self.chromosome_len() - 1)
-        offspring = N.r_[mama[:recomb], papa[recomb:]]
+        offsprings = self.sbx(mama, papa)
         
         # Possibly mutate:
-        which_genes = N.random.rand(self.chromosome_len()) < self._p_mute
-        if not any(which_genes):
-            return offspring
+        for offspring in offsprings:
+            which_genes = N.random.rand(self.chromosome_len()) <= self._p_mute
+            if not any(which_genes):
+                continue
         
-        # Polinomial mutation, see ref. [1]
-        delta = 1 - N.array((offspring[which_genes] - self._low_bnds[which_genes], \
-            self._up_bnds[which_genes] - offspring[which_genes])).min(axis=0) \
-            / self._ranges[which_genes]
+            # Polinomial mutation, see ref. [1]
+            delta = 1 - N.array((offspring[which_genes] - self._low_bnds[which_genes], \
+                self._up_bnds[which_genes] - offspring[which_genes])) \
+                / self._ranges[which_genes]
+                
+            mute_bases = N.random.random_sample(which_genes.sum())
+            perturb = N.empty_like(mute_bases)
+            close = mute_bases <= 0.5
+            close_ind = N.where(close)
+            far_ind = N.where(~close)
             
-        mute_bases = N.random.random_sample(which_genes.sum())
-        perturb = N.empty_like(mute_bases)
-        close = mute_bases <= 0.5
-        
-        perturb[close] = (2*mute_bases[close] + \
-            (1 - 2*mute_bases[close])*delta[close]**(self._et_m + 1))**(1./(self._et_m + 1)) - 1
-        perturb[~close] = 1 - (2*(1 - mute_bases[~close]) + \
-            2*(mute_bases[~close] - 0.5)*delta[~close]**(self._et_m + 1))**(1./(self._et_m + 1))
-        offspring[which_genes] += perturb*self._ranges[which_genes]
-        
-        return offspring
+            perturb[close] = (2*mute_bases[close] + \
+                (1 - 2*mute_bases[close])*delta[0,close_ind]**(self._et_m + 1))**(1./(self._et_m + 1)) - 1
+            perturb[~close] = 1 - (2*(1 - mute_bases[~close]) + \
+                2*(mute_bases[~close] - 0.5)*delta[1,far_ind]**(self._et_m + 1))**(1./(self._et_m + 1))
+            offspring[which_genes] += perturb*self._ranges[which_genes]
+            
+            underflow = offspring < self._low_bnds
+            offspring[underflow] = self._low_bnds[underflow]
+            overflow = offspring > self._up_bnds
+            offspring[overflow] = self._up_bnds[overflow]
+            
+        return offsprings
--- py_eps_moea/eps_moea.py
+++ py_eps_moea/eps_moea.py
@@ -36,14 +36,17 @@ def pop_select(fitness, archive):
     Returns: 
     the index of the subject selected for breeding.
     """
-    legible = N.where(~archive)[0]
-    compete = legible[N.random.random_integers(0, fitness.shape[0] - archive.sum() - 1, size=2)]
+    compete = N.random.random_integers(0, fitness.shape[0] - 1, size=2)
     
-    if (fitness[compete[0]] <= fitness[compete[1]]).all() and \
-        (fitness[compete[0]] < fitness[compete[1]]).any():
+    if (fitness[0] <= fitness[1]).all() and \
+        (fitness[0] < fitness[1]).any():
         return compete[0]
-    else:
+    if (fitness[1] <= fitness[0]).all() and \
+        (fitness[1] < fitness[0]).any():
         return compete[1]
+    if N.random.rand() < 0.5:
+        return compete[0]
+    return compete[1]
 
 def archive_select(archive_marker):
     """Selects for breeding an individual from the archive. Currently selects 
@@ -73,11 +76,18 @@ def pop_accept(fitness, contend_fit):
     repl - the index of the replaced subject, or None if no replacement occurred
     """
     underdogs = (contend_fit <= fitness).all(axis=1) & (contend_fit < fitness).any(axis=1)
-    if not underdogs.any():
+    if underdogs.any():
+        return N.where(underdogs)[0][0]
+        
+    # Is he dominated?
+    top_dogs = (contend_fit >= fitness).all(axis=1) & (contend_fit > fitness).any(axis=1)
+    if top_dogs.any():
         return None
-    return N.where(underdogs)[0][0]
-
-def archive_accept(archive, fitness, contend_fit, contend_idx, grid):
+    
+    # Non-domination: select at random.
+    return N.random.random_integers(0, fitness.shape[0] - 1)
+        
+def archive_accept(archive, fitness, grid_fit, contend_fit, grid_cont):
     """Update the pareto-front in the archive using epsilon-dominance.
     
     Arguments:
@@ -86,11 +96,9 @@ def archive_accept(archive, fitness, contend_fit, contend_idx, grid):
     fitness - a p by t array whose element (i,j) is the value of the jth fitness
         function of t functions, for the ith individual of p individuals, after the 
         last iteration of the population acceptance method.
+    grid_fit - the epsilon-fitness value of the population (p by t)
     contend_fit - the fitness of the contender.
-    contend_idx - the index in the population of the contender to replace some of 
-        the archive.
-    grid - a vector of length t specifying the edge-length of the n-dim grid in 
-        each of the fitness space axes.
+    grid_cont - a vector of length t with the epsilon-fitness of the contender
     
     Returns:
     accepted - a boolean value stating whether the contender was accepted.
@@ -98,59 +106,49 @@ def archive_accept(archive, fitness, contend_fit, contend_idx, grid):
     # We have more than one level of selection from the population,
     # so its convenient to work with indices rather than boolean arrays.
     archive_idxs = N.where(archive)[0]
-        
-    # Calculate the grid-fitness of the population:
-    dists = N.fmod(fitness[archive], grid)
-    grid_fit = fitness[archive] - dists
-    # and of the contender:
-    cont_dist = N.fmod(contend_fit, grid)
-    grid_cont = contend_fit - cont_dist
-    cont_dist_square = (cont_dist**2).sum()
+    arch_grid_fit = grid_fit[archive_idxs]
+    arch_fit = fitness[archive_idxs]
     
     # We look for opportunities to reject the contender, along the way removing 
     # any dominated member of the archive:
-    for vertex in map(N.array, set(tuple(row) for row in grid_fit)):
-        if (grid_cont == vertex).all():
+    for vertex in map(N.array, set(tuple(row) for row in arch_grid_fit)):
+        high = (grid_cont > vertex).any()
+        low = (grid_cont < vertex).any()
+        if not(high or low):
             # This hypercube may have non-dominated members.
-            in_grid = N.where((grid_fit == vertex).all(axis=1))[0]
+            in_grid = N.where((arch_grid_fit == vertex).all(axis=1))[0]
             
-            top_dogs = (fitness[archive_idxs[in_grid]] <= contend_fit).all(axis=1) & \
-                (fitness[archive_idxs[in_grid]] < contend_fit).any(axis=1)
-            if top_dogs.any():
-                archive[contend_idx] = False
+            lower_fit = (arch_fit[in_grid] < contend_fit).any(axis=1)
+            higher_fit = (arch_fit[in_grid] > contend_fit).any(axis=1)
+            if (lower_fit & ~higher_fit).any():
                 return False
+
+            archive[archive_idxs[in_grid]] = False
             
-            underdogs = (contend_fit <= fitness[archive_idxs[in_grid]]).all(axis=1) & \
-                (contend_fit < fitness[archive_idxs[in_grid]]).any(axis=1)
-            
-            archive[archive_idxs[in_grid[underdogs]]] = False
-            # Of the remaining solutions, the closest to the grid is taken:
+            underdogs = higher_fit & ~lower_fit
             if underdogs.all():
-                continue
-                
-            dist_squares = (dists[in_grid[~underdogs]]**2).sum(axis=1)
-            remaining = dist_squares < cont_dist_square
+                break
+            
+            # Of the remaining solutions, the closest to the grid is taken:                
+            dist_squares = ((arch_fit[in_grid[~underdogs]] - \
+                arch_fit[in_grid[~underdogs]])**2).sum(axis=1)
+            remaining = dist_squares <= ((contend_fit - grid_cont)**2).sum()
             
             if remaining.any():
                 archive[archive_idxs[in_grid[~underdogs][N.argmin(dist_squares)]]] = True
-                archive[contend_idx] = False
                 return False
-            else:
-                archive[archive_idxs[in_grid[~underdogs]]] = False
         
-        elif (grid_cont >= vertex).all() and (grid_cont > vertex).any():
+        elif high and not low:
             # The contender is dominated. The function can end, because
             # it's impossible that others in the archive may still be dominated.
-            archive[contend_idx] = False # make sure no leftovers from last iteration.
             return False
             
-        elif (grid_cont <= vertex).all() and (grid_cont < vertex).any():
+        elif low and not high:
             # This hypercube is dominated by the new solution, exclude all its
             # members from the archive:
-            in_grid = N.where((grid_fit == vertex).all(axis=1))[0]
+            in_grid = N.where((arch_grid_fit == vertex).all(axis=1))[0]
             archive[archive_idxs[in_grid]] = False
     
-    archive[contend_idx] = True
     return True
         
 def eps_moea_optimize(creature, pop_size, conv_gens, num_gens, objectives, grid):
@@ -177,31 +175,33 @@ def eps_moea_optimize(creature, pop_size, conv_gens, num_gens, objectives, grid)
     # Initial fitness:
     fitness = objectives(population)
     archive = pareto_front(fitness)
-    
+    grid_fit = fitness - N.fmod(fitness, grid)
+
     while (archive_stagnation < conv_gens) and (num_gens > 0):
         # Generate new solution:
         mama = pop_select(fitness, archive);
         papa = archive_select(archive);
-        offspring = creature.breed(population[mama], population[papa])
-        contend_fit = objectives(offspring)
+        offsprings = creature.breed(population[mama], population[papa])
+        for offspring in offsprings:
+            contend_fit = objectives(offspring)
+            grid_cont = contend_fit - N.fmod(contend_fit, grid)
         
-        # Accept the new solution to the population and archive:
-        repl = pop_accept(fitness, contend_fit)
+            # Accept the new solution to the population and archive:
+            accepted = archive_accept(archive, fitness, grid_fit, contend_fit, grid_cont)
+            repl = pop_accept(fitness, contend_fit)
+            
+            if accepted:
+                archive_stagnation = 0
+            if repl is None:
+                continue
+            
+            # Prepare next iteration:
+            population[repl] = offspring
+            fitness[repl] = contend_fit
+            archive[repl] = accepted
+            grid_fit[repl] = grid_cont
         
-        if repl is None:
-            archive_stagnation += 1
-            num_gens -= 1
-            continue
-        
-        accepted = archive_accept(archive, fitness, contend_fit, repl, grid)
-        
-        # Prepare next iteration:
-        fitness[repl] = contend_fit
-        population[repl] = offspring
-        
-        if accepted:
-            archive_stagnation = 0
-        else:
+        if not accepted: # any of the offsprings
             archive_stagnation += 1
         num_gens -= 1
     
@@ -220,12 +220,12 @@ if __name__ == "__main__":
             testfun = test_functions.tzd[int(sys.argv[1])]
         except IndexError:
             pass # Keep the default
-        
+    
     import time
     t = time.time()
-    grid = N.r_[0.05, 0.05]
+    grid = N.r_[0.0075, 0.0075]
     cr = Creature(N.zeros(30), N.ones(30), 0.033)
-    population, fitness, archive = eps_moea_optimize(cr, 100, 600, 10000, \
+    population, fitness, archive = eps_moea_optimize(cr, 100, 600, 20000, \
         testfun, grid)
     print time.time() - t
     
